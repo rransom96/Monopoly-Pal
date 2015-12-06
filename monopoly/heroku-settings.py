e5f9202ec1fb07ffc269d318c444c23f8cae28f5
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-12-05 20:49:59 -0800

    fixing static

--- monopoly/heroku-settings.py
+++ monopoly/heroku-settings.py
@@ -16,4 +16,9 @@ ALLOWED_HOSTS = ['*']
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
